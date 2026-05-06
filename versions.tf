038162b39cdb354353fea5c9d0bde859b8f88f04
raghavendiran222222-cloud <raghavendiran222222@gmail.com>
2026-05-06 14:57:02 +0530

    chore: pin terraform >=1.9 and azurerm/azapi provider versions

---
+++ versions.tf
@@ -0,0 +1,15 @@
+
+terraform {
+  required_version = ">= 1.9.0, < 2.0.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.71"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.9"
+    }
+  }
+}
