59addd5c4cf54d4f118a5a08b812b2e8ba4d4f46
raghavendiran222222-cloud <raghavendiran222222@gmail.com>
2026-05-08 09:24:22 +0530

    from scratch

--- examples/basic/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "vnet_id" { value = module.spoke_vnet.vnet_id }
-output "vnet_name" { value = module.spoke_vnet.vnet_name }
-output "subnet_ids" { value = module.spoke_vnet.subnet_ids }
-output "peerings" { value = module.spoke_vnet.peerings }
